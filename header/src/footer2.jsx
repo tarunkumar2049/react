fd8f470e60fa64ec38bdb47375a1f93fe941cfac
tarunkumar2049 <tarunkumar2024@gmail.com>
2026-08-25 18:52:34 +0530

    modified

--- header/src/footer2.jsx
+++ header/src/footer2.jsx
@@ -1,10 +1,28 @@
 import "./footer2.css"
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
 export default function Footer2() {
     return (
         <>
             <div className="footer">
-                <img className="footer-img" src="/src/assets/logo.svg" alt="logo" />
+                <div className="foot1">
+                    <img className="footer-img" src="/src/assets/logo.svg" alt="logo" />
+
+                    <span className="icon">
+                        <FaTwitterSquare />
+                        <FaFacebook />
+                        <FaInstagram />
+                    </span>
+
+                </div>
+                <div className="foot2">
+                    Made in WEBFLOW.2020.
+                </div>
             </div>
+
         </>
     )
 
